39f17c0cad1d608a1df94ff727fec9de119abb8b
jaidon82 <jaidon82@gmail.com>
2024-03-10 17:46:59 -0400

    2nd commit
    Added Unity Store Assets. Changed skybox. Made changes to Game Manager

--- ZombieReset/Assets/Scripts/GameManager.cs
+++ ZombieReset/Assets/Scripts/GameManager.cs
@@ -1,24 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-  public static GameManager Instance;
+    public static GameManager Instance;
     public GameObject player;
     public playerController playerScript;
 
     [SerializeField] GameObject menuPause;
-    void Start()
+    public bool isPaused;
+    void Awake()
     {
-       Instance = this;
+        Instance = this;
         player = GameObject.FindWithTag("Player");
-        playerScript= player.GetComponent<playerController>();
+        playerScript = player.GetComponent<playerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Cancel"))
+        {
+
+            menuPause.SetActive(isPaused);
+        }
+
+
     }
+
+    public void statePaused()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0.0f;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void stateUnpaused()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 1.0f;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+
+
 }
